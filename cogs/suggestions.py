0ca350418065c49de1458498725dae0eb3800b2e
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-01-26 12:17:07 +0000

    Embeded most of the messages and all error messages

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -39,20 +39,36 @@ class Suggestions(commands.Cog):
 
         for role in ctx.message.author.roles:
             if role.name == "Suggestion Blacklist":
-                return await ctx.message.reply("You are blacklisted from suggestions!")
+                embed = discord.Embed(color=discord.Colour.red(),
+                                      description="You are blacklisted from suggestions!")
+                embed.set_author(name="Error", 
+                                 icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+                return await ctx.message.reply(embed=embed)
             else:
                 continue
 
         if await self.check_suggestion_channel(ctx.message.guild) is None:
-            return await ctx.message.reply("This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         else:
             channel = await self.check_suggestion_channel(ctx.message.guild)
         
         if suggestion is None:
-            return await ctx.message.reply("You did not provide a suggestion! To use this command run ```s!suggest <suggestion>```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not provide a suggestion! To use this command run ```s!suggest <suggestion>```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         
         if (len(suggestion) < 16):
-            return await ctx.message.reply("You cannot possibly explain your suggestion in so few words - please try again!")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You cannot possibly explain your suggestion in so few words - please try again!")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         
         webhook = await self.get_webhook(channel)
         if webhook is None:
@@ -78,14 +94,32 @@ class Suggestions(commands.Cog):
     
     @commands.command(name="editsuggestion")
     async def _editsuggestion(self, ctx, messageid: int = None, *, newcontent = None):
+
+        for role in ctx.message.author.roles:
+            if role.name == "Suggestion Blacklist":
+                embed = discord.Embed(color=discord.Colour.red(),
+                                      description="You are blacklisted from suggestions!")
+                embed.set_author(name="Error",
+                                 icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+                return await ctx.message.reply(embed=embed)
+            else:
+                continue
         
         if await self.check_suggestion_channel(ctx.message.guild) is None:
-            return await ctx.message.reply("This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         else:
             channel = await self.check_suggestion_channel(ctx.message.guild)
         
         if messageid is None:
-            return await ctx.message.reply("You did not use this command correctly. The correct implementaton is ```s!editsuggestion [Message ID] <Reason>```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not use this command correctly. The correct implementaton is ```s!editsuggestion [Message ID] <Reason>```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
 
         message = await channel.fetch_message(messageid)
 
@@ -113,14 +147,32 @@ class Suggestions(commands.Cog):
     
     @commands.command(name="deletesuggestion")
     async def _deletesuggestion(self, ctx, messageid: int = None):
+
+        for role in ctx.message.author.roles:
+            if role.name == "Suggestion Blacklist":
+                embed = discord.Embed(color=discord.Colour.red(),
+                                      description="You are blacklisted from suggestions!")
+                embed.set_author(name="Error",
+                                 icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+                return await ctx.message.reply(embed=embed)
+            else:
+                continue
         
         if await self.check_suggestion_channel(ctx.message.guild) is None:
-            return await ctx.message.reply("This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         else:
             channel = await self.check_suggestion_channel(ctx.message.guild)
         
         if messageid is None:
-            return await ctx.message.reply("You did not use this command correctly. The correct implementaton is ```s!deletesuggestion [Message ID]```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not use this command correctly. The correct implementaton is ```s!deletesuggestion <Message ID>```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
 
         message = await channel.fetch_message(messageid)
 
@@ -129,7 +181,7 @@ class Suggestions(commands.Cog):
             webhook = await channel.create_webhook(name="Suggestion Webhook")
 
         if str(ctx.message.author.id) in message.embeds[0].footer.text:
-            await message.clear_reactions()
+            
             async with aiohttp.ClientSession() as session:
                 real_webhook = Webhook.from_url(webhook.url, adapter=AsyncWebhookAdapter(session))
                 await real_webhook.edit_message(messageid, embed=None, content="This suggestion has been deleted.")
@@ -145,12 +197,20 @@ class Suggestions(commands.Cog):
     async def _accept(self, ctx, messageid: int = None, *, reason = "No Reason"):
         
         if await self.check_suggestion_channel(ctx.message.guild) is None:
-            return await ctx.message.reply("This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         else:
             channel = await self.check_suggestion_channel(ctx.message.guild)
 
         if messageid is None:
-            return await ctx.message.reply("You did not use this command correctly. The correct implementaton is ```s!accept [Message ID] <Reason>```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not use this command correctly. The correct implementaton is ```s!accept <Message ID> [Reason]```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
 
         message = await channel.fetch_message(messageid)
                 
@@ -179,19 +239,31 @@ class Suggestions(commands.Cog):
     @_accept.error 
     async def _accept_handler(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.message.reply("You are missing the permissions needed to use this command!", delete_after=3.0)
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You are missing the permissions needed to use this command!")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed, delete_after=5.0)
     
     @commands.has_permissions(manage_guild=True)
     @commands.command(name="deny")
     async def _deny(self, ctx, messageid: int = None, *, reason = "No Reason"):
         
         if await self.check_suggestion_channel(ctx.message.guild) is None:
-            return await ctx.message.reply("This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="This guild has no suggestion channel to fix this either:\n > **1.** Make a channel called `#suggestions`\n > **2.**  Run the command `s!setup`")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         else:
             channel = await self.check_suggestion_channel(ctx.message.guild)
 
         if messageid is None:
-            return await ctx.message.reply("You did not use this command correctly. The correct implementaton is ```s!deny [Message ID] <Reason>```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not use this command correctly. The correct implementaton is ```s!deny [Message ID] <Reason>```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
 
         message = await channel.fetch_message(messageid)
         
@@ -220,14 +292,22 @@ class Suggestions(commands.Cog):
     @_deny.error 
     async def _deny_handler(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.message.reply("You are missing the permissions needed to use this command!", delete_after=3.0)
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You are missing the permissions needed to use this command!")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed, delete_after=5.0)
 
     @commands.has_permissions(manage_guild=True)
     @commands.command(name="setup")
     async def _setup(self, ctx):
         for tchannel in ctx.guild.text_channels:
             if tchannel.name == "suggestions":
-                return await ctx.message.reply("There is already a suggestion channel!")
+                embed = discord.Embed(color=discord.Colour.red(),
+                                  description="There is already a suggestion channel!")
+                embed.set_author(name="Error",
+                                 icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+                return await ctx.message.reply(embed=embed)
             else:
                 continue
         
@@ -240,22 +320,38 @@ class Suggestions(commands.Cog):
         
         emoji = "<:yes:802083268098785291>"
         await ctx.message.add_reaction(emoji)
-        await channel.send("**__This is the new suggestion channel__**\n\nIf you type `s!suggest <Suggestion>` anywhere in this server it will pop up here! \n\nMembers who have **Manage Server** permissions can accept and deny suggestions. They can also blacklist people from suggestions. \n\nI hope you enjoy this bot!")
+        embed = discord.Embed(color=discord.Colour.gold(),
+                              description="**__This is the new suggestion channel__**\n\nIf you type `s!suggest <Suggestion>` anywhere in this server it will pop up here! \n\nMembers who have **Manage Server** permissions can accept and deny suggestions. They can also blacklist people from suggestions. \n\nI hope you enjoy this bot!")
+        embed.set_author(name="New Suggestion Channel",
+                         icon_url="https://cdn.discordapp.com/avatars/743435729085923379/a1a5d77e6413466d58f95691eb290b6e.webp?size=128")
+        return await ctx.message.reply(embed=embed)
 
     @_setup.error 
     async def _setup_handler(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.message.reply("You are missing the permissions needed to use this command!", delete_after=3.0)
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You are missing the permissions needed to use this command!")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed, delete_after=5.0)
 
     @commands.has_permissions(manage_guild=True)
     @commands.command(name="blacklist")
     async def _blacklist(self, ctx, user: discord.Member = None):
         if user is None:
-            return await ctx.message.reply("You did not use this command correctly. The correct implementation for that command is ```s!blacklist <Member>```")
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You did not use this command correctly. The correct implementation for that command is ```s!blacklist <Member>```")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed)
         
         for role in user.roles:
             if role.name == "Suggestion Blacklist":
-                return await ctx.message.reply("They are already blacklisted!")
+                embed = discord.Embed(color=discord.Colour.red(),
+                                      description="You did not use this command correctly. The correct implementation for that command is ```s!blacklist <Member>```")
+                embed.set_author(name="Error",
+                                 icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+                return await ctx.message.reply(embed=embed)
         
         for role in ctx.guild.roles:
             if role.name == "Suggestion Blacklist":
@@ -270,7 +366,11 @@ class Suggestions(commands.Cog):
     @_blacklist.error 
     async def _blacklist_handler(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.message.reply("You are missing the permissions needed to use this command!", delete_after=3.0)
+            embed = discord.Embed(color=discord.Colour.red(),
+                                  description="You are missing the permissions needed to use this command!")
+            embed.set_author(name="Error",
+                             icon_url="https://cdn.discordapp.com/emojis/802083299559473222.png")
+            return await ctx.message.reply(embed=embed, delete_after=5.0)
 
 def setup(bot):
     bot.add_cog(Suggestions(bot))
